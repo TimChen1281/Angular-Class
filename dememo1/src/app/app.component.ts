7fe9189a3d53bf5ade330691cf85ebf8a08f4cf6
timmy <z128137694@gmail.com>
2022-05-22 23:50:34 +0800

    新增新專案

--- dememo1/src/app/app.component.ts
+++ dememo1/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'dememo1';
+  keyword ='123';
+  data:any[] = [];
+
+  constructor(private http:HttpClient) { }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.http.get<any[]>('/api/articles.json').subscribe({
+      next: (result)=>{
+        this.data = result;
+      }
+    })
+  }
+
+  changeKeyword(str:string) {
+    this.keyword = str;
+  }
 }
--- dememo1/src/app/app.module.ts
+++ dememo1/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { MaskPipe } from './mask.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MaskPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
